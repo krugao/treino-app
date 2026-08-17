86725702682df68f2628d213314451a3fb8d67d5
krugao <leona@users.noreply.github.com>
2026-08-17 20:28:12 -0300

    Insights: recovery score, correlation, alerts, adjust log, weekly report; Polar on phone

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
 self.addEventListener("install", e => {
-  e.waitUntil(caches.open("treino-v5").then(c => c.addAll(["./", "./index.html", "./manifest.json"])));
+  e.waitUntil(caches.open("treino-v6").then(c => c.addAll(["./", "./index.html", "./manifest.json"])));
   self.skipWaiting();
 });
 self.addEventListener("activate", e => e.waitUntil(
-  caches.keys().then(keys => Promise.all(keys.filter(k => k !== "treino-v5").map(k => caches.delete(k))))
+  caches.keys().then(keys => Promise.all(keys.filter(k => k !== "treino-v6").map(k => caches.delete(k))))
     .then(() => self.clients.claim())
 ));
 self.addEventListener("fetch", e => {
