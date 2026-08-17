802975d591224ca440f03f9400f3c01d078e1cdd
krugao <leona@users.noreply.github.com>
2026-08-17 19:47:38 -0300

    Bump service worker cache to pick up routine

--- sw.js
+++ sw.js
@@ -1,8 +1,11 @@
 self.addEventListener("install", e => {
-  e.waitUntil(caches.open("treino-v1").then(c => c.addAll(["./", "./index.html", "./manifest.json"])));
+  e.waitUntil(caches.open("treino-v2").then(c => c.addAll(["./", "./index.html", "./manifest.json"])));
   self.skipWaiting();
 });
-self.addEventListener("activate", e => e.waitUntil(self.clients.claim()));
+self.addEventListener("activate", e => e.waitUntil(
+  caches.keys().then(keys => Promise.all(keys.filter(k => k !== "treino-v2").map(k => caches.delete(k))))
+    .then(() => self.clients.claim())
+));
 self.addEventListener("fetch", e => {
   e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
 });
