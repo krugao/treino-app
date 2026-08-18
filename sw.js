21aeb642de7fe07168a8053bece15cb37dc7f817
Your Name <krugao@users.noreply.github.com>
2026-08-17 21:17:57 -0300

    Fix dead buttons: close liveAlerts brace and bump cache to v8

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
 self.addEventListener("install", e => {
-  e.waitUntil(caches.open("treino-v7").then(c => c.addAll(["./", "./index.html", "./manifest.json"])));
+  e.waitUntil(caches.open("treino-v8").then(c => c.addAll(["./", "./index.html", "./manifest.json"])));
   self.skipWaiting();
 });
 self.addEventListener("activate", e => e.waitUntil(
-  caches.keys().then(keys => Promise.all(keys.filter(k => k !== "treino-v7").map(k => caches.delete(k))))
+  caches.keys().then(keys => Promise.all(keys.filter(k => k !== "treino-v8").map(k => caches.delete(k))))
     .then(() => self.clients.claim())
 ));
 self.addEventListener("fetch", e => {
