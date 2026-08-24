1ee7e513b0e3fb8662948f82266ab4b3888daee9
Your Name <leonardohenriquebk@gmail.com>
2026-08-24 18:36:46 -0300

    Update routine, diet, and supplement protocol on the phone site.

--- sw.js
+++ sw.js
@@ -1,11 +1,17 @@
 self.addEventListener("install", e => {
-  e.waitUntil(caches.open("treino-v8").then(c => c.addAll(["./", "./index.html", "./manifest.json"])));
+  e.waitUntil(caches.open("treino-v9").then(c => c.addAll(["./", "./index.html", "./manifest.json"])));
   self.skipWaiting();
 });
 self.addEventListener("activate", e => e.waitUntil(
-  caches.keys().then(keys => Promise.all(keys.filter(k => k !== "treino-v8").map(k => caches.delete(k))))
+  caches.keys().then(keys => Promise.all(keys.filter(k => k !== "treino-v9").map(k => caches.delete(k))))
     .then(() => self.clients.claim())
 ));
 self.addEventListener("fetch", e => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+  e.respondWith(
+    fetch(e.request).then(r => {
+      const copy = r.clone();
+      caches.open("treino-v9").then(c => c.put(e.request, copy));
+      return r;
+    }).catch(() => caches.match(e.request))
+  );
 });
